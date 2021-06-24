5b10e603a8b31aa84a9a1ee16d7c071f590f2486
Tawatwee Aiemarreerat <punnyoz1103@gmail.com>
2021-06-24 09:25:46 +0700

    fixes

--- app.py
+++ app.py
@@ -10,6 +10,9 @@ app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
+app = Flask(__name__)
+app.secret_key = 'HiTherePun'
+
 @app.route("/")
 def index():
 
